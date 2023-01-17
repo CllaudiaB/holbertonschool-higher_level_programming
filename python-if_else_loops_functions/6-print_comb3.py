4e5d556d374a68fa22f13f0bf4b21109c71fa759
CllaudiaB <buraanna27@gmail.com>
2023-01-17 01:49:14 -0800

    fixed syntax error 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(0, 9):
         if i == 8 and j == 9:
             print("{:d}{:d}".format(i, j))
         else:
-            print("{:d}{:d}, ".format(i, j), end ='')
+            print("{:d}{:d}, ".format(i, j), end='')
