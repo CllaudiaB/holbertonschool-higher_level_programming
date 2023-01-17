011d7cc0a908d69dc2fa54b301c05f89dc14e2e3
CllaudiaB <buraanna27@gmail.com>
2023-01-17 05:25:35 -0800

    adding 8-uppercase.py

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        n = ord(str[i])
+        if n >= 97 and n <= 122:
+            n = n - 32
+        l = chr(n)
+        if i == len(str) - 1:
+            print("{:s}".format(l))
+        else:
+            print("{:s}".format(l), end='')
