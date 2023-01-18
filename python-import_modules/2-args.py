ba078e6addce7f3e0309fef141099571a36e8e64
CllaudiaB <buraanna27@gmail.com>
2023-01-18 08:01:12 -0800

    adding 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-        import sys
+    import sys
 
-        for i in range(0, len(sys.argv)):
-                i = i + 1
+    for i in range(0, len(sys.argv)):
+        i = i + 1
 
-        i = i - 1
+    i = i - 1
 
-        if i == 1:
-            print("{} argument:".format(i))
-        elif i == 0:
-            print("{} arguments.".format(i))
-        else:
-            print("{} arguments:".format(i))
+    if i == 1:
+        print("{} argument:".format(i))
+    elif i == 0:
+        print("{} arguments.".format(i))
+    else:
+        print("{} arguments:".format(i))
 
-        for a in range(0, len(sys.argv)):
-            if a != 0:
-                print("{}: {}".format(a, sys.argv[a]))
+    for a in range(0, len(sys.argv)):
+        if a != 0:
+           print("{}: {}".format(a, sys.argv[a]))
