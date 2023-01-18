43260c6d1c5571f6c5a1dd8575db1d4f17b7cf68
CllaudiaB <buraanna27@gmail.com>
2023-01-18 08:10:26 -0800

    fixed syntax error

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     for i in range(0, len(sys.argv)):
         i = i + 1
-
-    i = i - 1
-
-    if i == 1:
-        print("{} argument:".format(i))
-    elif i == 0:
-        print("{} arguments.".format(i))
-    else:
-        print("{} arguments:".format(i))
-
+     i = i - 1
+     if i == 1:
+         print("{} argument:".format(i))
+     elif i == 0:
+         print("{} arguments.".format(i))
+     else:
+         print("{} arguments:".format(i))
     for a in range(0, len(sys.argv)):
         if a != 0:
-           print("{}: {}".format(a, sys.argv[a]))
+            print("{}: {}".format(a, sys.argv[a]))
