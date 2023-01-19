29a6fefc791d22c6fb58dcf1ec02acd77193e3e4
CllaudiaB <buraanna27@gmail.com>
2023-01-19 09:10:25 -0800

    adding 101-remove_char_at.py

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    for i in range(len(str)):
+        if i != n:
+            print("{}".format(str[i]), end='')
+        else:
+            continue
+    return(" ")
