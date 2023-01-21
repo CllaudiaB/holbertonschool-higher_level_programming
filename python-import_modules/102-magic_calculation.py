cd6bb66e1d6faaf48e0b68647186524e7f97085a
CllaudiaB <buraanna27@gmail.com>
2023-01-21 07:32:12 -0800

    adding magic_calculation_102.py & 102-magic_calculation.py

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -6,8 +6,8 @@ def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-    else:
         for i in range(4, 6):
             c = add(c, i)
             return c
-        sub(a, b)
+    else:
+        return (sub(a, b))
