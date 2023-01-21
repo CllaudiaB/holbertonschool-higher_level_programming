c5f98341f64ef13d1378842526f11e01328831bb
CllaudiaB <buraanna27@gmail.com>
2023-01-21 05:19:44 -0800

    adding 102-magic_calculation.py

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 import dis
+
+
 def magic_calculation(a, b, c):
     if a < b:
         return c
     elif c > b:
         return (a + b)
     else:
-        return ((a + b) - c)
+        return ((a * b) - c)
+dis.dis(magic_calculation)
