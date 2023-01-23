c324b2aaf292d2e0ed588123305791e3f38e08a7
CllaudiaB <buraanna27@gmail.com>
2023-01-23 12:29:35 -0800

    adding 7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,8 +2,8 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_c = ()
     if len(tuple_a) < 2 or len(tuple_b) < 2:
-        tuple_a += 0, 0
-        tuple_b += 0, 0
+        tuple_a += (0, 0)
+        tuple_b += (0, 0)
         tuple_c = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
         return (tuple_c)
     else:
