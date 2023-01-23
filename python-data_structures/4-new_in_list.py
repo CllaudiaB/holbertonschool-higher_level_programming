1087a81a3862f9bf279cf11dd9874e6f18a0502d
CllaudiaB <buraanna27@gmail.com>
2023-01-23 06:12:18 -0800

    adding 4-new_in_list.py

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
+    if idx < 0:
+        return(new_list)
+    elif idx not in range(len(my_list)):
+        return(new_list)
+    else:
+        new_list[idx] = element
+        return(new_list)
