c8efb2a5c738102081a28381f05247b869f93e9c
CllaudiaB <buraanna27@gmail.com>
2023-01-23 23:56:55 -0800

    adding 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
+    if sentence == "":
+        first = None
+    else:
+        first = sentence[0]
     tuple_a = (length, first)
     return (tuple_a)
