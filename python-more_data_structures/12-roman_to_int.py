e96c2cab502a44f1c0ddaa2145f755ab221e278a
CllaudiaB <buraanna27@gmail.com>
2023-01-26 11:42:41 -0800

    adding 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,8 @@ def roman_to_int(roman_string):
                   'M': 1000}
     value = 0
     if type(roman_string) != str or not dictionary_r:
-        return 0
+        return
+
     for i, element in enumerate(roman_string):
         if i != (len(roman_string) - 1) and dictionary_r[element]\
            < dictionary_r[roman_string[i + 1]]:
