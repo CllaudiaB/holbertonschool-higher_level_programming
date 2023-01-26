937ea9fd5c6f3a9546d723a02f82ba29d3e8a724
CllaudiaB <buraanna27@gmail.com>
2023-01-26 11:50:25 -0800

    adding 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
 
-    dictionary_r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,\
-                  'M': 1000}
+    d_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     value = 0
-    if type(roman_string) != str or not dictionary_r:
+    if type(roman_string) != str or not d_roman:
         return 0
 
     for i, element in enumerate(roman_string):
-        if i != (len(roman_string) - 1) and dictionary_r[element]\
-           < dictionary_r[roman_string[i + 1]]:
-            value -= dictionary_r[element]
+        if i != (len(roman_string) - 1) and d_roman[element]\
+           < d_roman[roman_string[i + 1]]:
+            value -= d_roman[element]
         else:
-            value += dictionary_r[element]
+            value += d_roman[element]
     return value
