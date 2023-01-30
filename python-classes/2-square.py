07e0dd723d4b0a90c4eed83033780f6dfbe83b16
CllaudiaB <buraanna27@gmail.com>
2023-01-30 08:13:36 -0800

    adding 2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Defines a square
+"""
+
+class Square:
+    """
+    That defines a square by: based on 1-square.py
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
