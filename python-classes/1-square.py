545802224fc439f337a3a84b97b3ef6e5e951122
CllaudiaB <buraanna27@gmail.com>
2023-01-30 07:28:13 -0800

    adding 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines a square
+"""
+
+class Square:
+    """
+    that defines a square by: based on 0-square.py
+    """
+    def __init__(self, size):
+        self.__size = size
