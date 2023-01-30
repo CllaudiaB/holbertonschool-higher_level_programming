ae87dd46692c2f47fbcfa9b9b533acfd1dc36e82
CllaudiaB <buraanna27@gmail.com>
2023-01-30 07:33:22 -0800

    fixed PEP8 file  1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,7 +6,7 @@ Defines a square
 
 class Square:
     """
-    that defines a square by: based on 0-square.py
+    That defines a square by: based on 0-square.py
     """
     def __init__(self, size):
         self.__size = size
