cdaba31b21f4797376e04a8628ed9add18f57356
CllaudiaB <buraanna27@gmail.com>
2023-01-30 08:48:34 -0800

    adding 3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Defines a square
+"""
+
+
+class Square:
+    """
+    That defines a square by: (based on 2-square.py)
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    """
+    That returns the current square area
+    """
+    def area(self):
+        square_area = self.__size
+        return square_area * square_area
