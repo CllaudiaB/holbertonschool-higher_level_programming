247459935a3e2cee75901c3e3a3d0f21663658be
CllaudiaB <buraanna27@gmail.com>
2023-01-30 11:41:43 -0800

    adding 6-square.py

---
+++ python-classes/6-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Defines a square
+"""
+
+
+class Square:
+    """
+    That defines a square by: based on 5-square.py
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+        def area(self):
+            """
+            Return the current square area
+            """
+            square_area = self.__size
+            return square_area * square_area
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(position) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+
+            for i in range(self.__size):
+                for j in range(self.position[0]):
+                    print(' ', end='')
+                for k in range(self.__size):
+                    print('#', end='')
+                print()
