dcdd83cb269b7233fc9fc9623d93765f918d4b48
CllaudiaB <buraanna27@gmail.com>
2023-02-01 00:36:06 -0800

    fixed comparison error

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,9 +8,9 @@ def add_integer(a, b=98):
     """
     Function that adds 2 integers
     """
-    if type(a) is not int or type(a) is not float:
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) is not int or type(b) is not float:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
     if type(a) == float:
