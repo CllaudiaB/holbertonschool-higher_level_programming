50697f3942a063ae67256b5ae78a226745c9de47
CllaudiaB <buraanna27@gmail.com>
2023-02-01 06:13:14 -0800

    adding tests/4-print_square.txt && 4-print_square.py

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Print square
+"""
+
+
+def print_square(size):
+    """
+    Function that prints a square with the character #
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size < 0 and type(size) is float:
+        raise TypeError("size must be an integer")
+    if size is None:
+        raise TypeError("print_square() missing 1 required positional argument: \
+'size'")
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print()
