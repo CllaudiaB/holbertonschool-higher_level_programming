058bd8cd38936c7120b2ecec51f95a8338cf8c9f
CllaudiaB <buraanna27@gmail.com>
2023-02-02 05:06:15 -0800

    TypeError

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
         if type(width) is not int:
             raise TypeError("width must be an integer")
