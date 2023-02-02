44666ce5b816c9292a386a629da6a9156d401b14
CllaudiaB <buraanna27@gmail.com>
2023-02-02 04:22:38 -0800

    adding 1-rectangle.py

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Real definition of a rectangle
+"""
+class Rectangle:
+    """
+     That defines a rectangle by: based on 0-rectangle.py
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__heihgt = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
