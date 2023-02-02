a4233c92906d7c2c7e774c752d28604c271f2c77
CllaudiaB <buraanna27@gmail.com>
2023-02-02 10:04:03 -0800

    adding 4-rectangle.py

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -54,7 +54,7 @@ class Rectangle:
             for j in range(self.__width):
                 rectangle += '#'
             rectangle += '\n'
-        return rectangle[:-1]
+        return rectangle[:]
 
     def __repr__(self):
         return "Rectangle ({}, {})".format(self.__width, self.__height)
