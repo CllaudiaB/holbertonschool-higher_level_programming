c50ab8a002112cdf920d27330e3fbf3b2caa9f24
CllaudiaB <buraanna27@gmail.com>
2023-02-02 05:17:26 -0800

    TypeError

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -32,7 +32,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if isinstance(value, int) is False:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__heihgt = value
