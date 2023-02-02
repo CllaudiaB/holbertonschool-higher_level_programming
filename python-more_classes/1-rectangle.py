95878a7c6bf1bef4580a3af79b1d0a4594fe31b7
CllaudiaB <buraanna27@gmail.com>
2023-02-02 04:35:06 -0800

    fixed TypeError

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -13,6 +13,16 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
     @property
     def width(self):
         return self.__width
@@ -20,10 +30,6 @@ class Rectangle:
     @width.setter
     def width(self, value):
         self.__width = value
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
 
     @property
     def height(self):
@@ -32,7 +38,3 @@ class Rectangle:
     @height.setter
     def height(self, value):
         self.__heihgt = value
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
