7cd55555cebe55969ca0e11929f362d75e9f79d6
CllaudiaB <buraanna27@gmail.com>
2023-02-07 09:50:35 -0800

    adding 4-inherits_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Only sub class of
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Function that returns True if the object is an instance of a class that \
+    inherited (directly or indirectly) from the specified class ; \
+    otherwise False
+    """
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
