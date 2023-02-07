944bf3cea65ed184649f4404e6cc920fdb2c63b5
CllaudiaB <buraanna27@gmail.com>
2023-02-07 06:30:47 -0800

    adding 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+Lookup
+"""
 def lookup(obj):
+    """
+    Function that returns the list of available attributes and methods \
+    of an object
+    """
     return dir(obj)
