971a4f197e3a005fc9e4ca488fe23ec365df782e
CllaudiaB <buraanna27@gmail.com>
2023-02-08 03:15:18 -0800

    adding tests/7-base_geometry.txt && 7-base_geometry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Integer validator
+"""
+
+
+class BaseGeometry:
+    """
+    Write a class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be un integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
