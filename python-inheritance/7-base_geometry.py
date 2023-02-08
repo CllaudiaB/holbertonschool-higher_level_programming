f83b1a374f6099fe8d42aaf5503862853a9aeda3
CllaudiaB <buraanna27@gmail.com>
2023-02-08 03:23:49 -0800

    fixed test error

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,6 +13,6 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         if type(value) is not int:
-            raise TypeError("{} must be un integer".format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
