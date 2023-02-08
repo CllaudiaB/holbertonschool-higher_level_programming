e9a85cf3f1e1fe42064c2b31e44717aef9f65602
CllaudiaB <buraanna27@gmail.com>
2023-02-08 06:32:40 -0800

    adding 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -9,7 +9,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Squrae(Rectangle):
     """
-    Class Square that inherits from Rectangle and return area
+    Class Square
     """
     def __init__(self, size):
         self.__size = size
