c1d4564e751959a8e4b1a67cc619d2b535c0653f
CllaudiaB <buraanna27@gmail.com>
2023-02-13 06:42:36 -0800

    adding models/rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -74,8 +74,12 @@ class Rectangle(Base):
         """
         That prints in stdout the Rectangle instance with the character #
         """
+        for i in range(self.__x):
+            print()
         for i in range(self.__height):
-            for j in range(self.__width):
+            for j in range(self.__y):
+                print(' ', end='')
+            for k in range(self.__width):
                 print('#', end='')
             print()
 
