5e466143c5a9329c35adc5b8dc13a08714b0ded8
CllaudiaB <buraanna27@gmail.com>
2023-02-13 04:00:56 -0800

    adding models/rectangle.py

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+First Rectangle
+"""
+from models.base import Base
+
+
+
+class Rectangle(Base):
+    """
+    Write the class Rectangle that inherits from Base
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
