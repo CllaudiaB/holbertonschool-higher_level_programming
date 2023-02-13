0e5078b012451f0e591efd8b033028f97e86a05c
CllaudiaB <buraanna27@gmail.com>
2023-02-13 06:17:01 -0800

    adding models/rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -83,5 +83,5 @@ class Rectangle(Base):
         """
          Returns [Rectangle] (<id>) <x>/<y> - <width>/<height>
         """
-        return "[Rectangle]({}) {}/{} - {}/{}".format(self.id, self.__x, \
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, \
                                         self.__y, self.__width, self.__height)
