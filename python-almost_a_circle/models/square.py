0e073422e871581f39432a0028fb8a50d7abfae0
CllaudiaB <buraanna27@gmail.com>
2023-02-14 02:23:59 -0800

    adding models/square.py

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+And now, the Square!
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Write the class Square that inherits from Rectangle
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
