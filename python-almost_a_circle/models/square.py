d415b05be27b7a3e6b56c5ab3cb322bab012b0d4
CllaudiaB <buraanna27@gmail.com>
2023-02-14 06:15:54 -0800

    adding models/square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -50,3 +50,14 @@ class Square(Rectangle):
                     self.x = value
                 if key == "y":
                     self.y = value
+
+        def to_dictionary(self):
+            """
+            Returns the dictionary representation of a Square
+            """
+            return {
+                "id": self.id,
+                "size": self.size,
+                "x": self.__x,
+                "y": self.__y
+                }
