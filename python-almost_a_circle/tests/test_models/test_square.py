fdd58208ad42036211e3d03627dcb48d61e4a3b2
CllaudiaB <buraanna27@gmail.com>
2023-02-15 12:07:44 -0800

    adding file

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -13,11 +13,11 @@ class Test_Square(unittest.TestCase):
         Test instance attributes
         """
         s = Square(8, 6, 4, 18)
-        self.assertEqual(r.id, 18)
-        self.assertEqual(r.size, 8)
-        self.assertEqual(r.x, 6)
-        self.assertEqual(r.y, 4)
+        self.assertEqual(s.id, 18)
+        self.assertEqual(s.size, 8)
+        self.assertEqual(s.x, 6)
+        self.assertEqual(s.y, 4)
         s = Square(1, 2, 3)
-        self.assertEqual(r.size, 1)
-        self.assertEqual(r.x, 2)
-        self.assertEqual(r.y, 3)
+        self.assertEqual(s.size, 1)
+        self.assertEqual(s.x, 2)
+        self.assertEqual(s.y, 3)
