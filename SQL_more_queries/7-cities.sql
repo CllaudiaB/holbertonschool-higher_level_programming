ad8bfd3fe61ae51bef283b0624fc2767a2bbc867
CllaudiaB <buraanna27@gmail.com>
2023-03-14 09:48:05 -0700

    fixed error

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,8 +3,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
-    PRIMARY KEY (id),
     state_id INT NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id),
     name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
