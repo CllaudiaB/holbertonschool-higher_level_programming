aaeae46e1c35421961efea2b0e1bba1255b11f89
CllaudiaB <buraanna27@gmail.com>
2023-03-14 13:01:10 -0700

    adding file for task 10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,3 @@
+-- script that lists all shows contained in hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
