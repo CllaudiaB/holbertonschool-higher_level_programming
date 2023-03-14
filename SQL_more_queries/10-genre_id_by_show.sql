703f42704f09f66294b33c9246ca58d5a520f285
CllaudiaB <buraanna27@gmail.com>
2023-03-14 15:04:57 -0700

    fixed error

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,6 @@
 -- script that lists all shows contained in hbtn_0d_tvshows
-SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+     ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
