28a6cd6a673ca534a84ecfed6e8e7d35c15180cf
CllaudiaB <buraanna27@gmail.com>
2023-03-18 09:42:38 -0700

    fixed error

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""lists all State objects from the database hbtn_0e_6_usa"""
-
+"""
+that lists all State objects from the database hbtn_0e_6_usa
+"""
 import sys
 from model_state import State, Base
 from sqlalchemy import create_engine
