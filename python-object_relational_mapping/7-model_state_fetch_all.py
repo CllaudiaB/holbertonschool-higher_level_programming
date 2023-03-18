da77956a52e9cfea32bfd5cbc13224e4767a9d2c
CllaudiaB <buraanna27@gmail.com>
2023-03-18 09:49:57 -0700

    fixed indentation problem

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Write a script that lists all State objects from the database hbtn_0e_6_usa
-"""
+""" lists all State objects from the database hbtn_0e_6_usa """
 import sys
 from model_state import State, Base
 from sqlalchemy import create_engine
@@ -12,9 +10,9 @@ if __name__ == '__main__':
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-for state in session.query(State).order_by(State.id).all():
-    print("{}: {}".format(state.id, state.name))
-session.close()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
