afb54187c829947c09722eac51301866c1a578ee
CllaudiaB <buraanna27@gmail.com>
2023-03-18 04:51:58 -0700

    adding file from task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script that lists all states with a name starting with N (upper N)
+   from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host ="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
