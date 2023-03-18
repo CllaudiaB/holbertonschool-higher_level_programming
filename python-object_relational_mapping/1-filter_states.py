1e05abb534cb0139e2429e45a64de5586bd45b36
CllaudiaB <buraanna27@gmail.com>
2023-03-18 05:40:55 -0700

    fixed error

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,14 +7,15 @@ import sys
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(
-        host ="localhost",
+        host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+    ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
