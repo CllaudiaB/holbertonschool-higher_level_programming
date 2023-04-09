dd7a63bd3d271177c7ee8966b40bfbb5288fadb8
CllaudiaB <buraanna27@gmail.com>
2023-04-09 11:23:26 -0700

    adding file for task 2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+	    this.width;
+	    this.hight;
+    } else {
+	    this.width = w;
+	    this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
