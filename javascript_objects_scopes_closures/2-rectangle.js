59e9183e7f07c940297272728da83e5db6b79fe7
CllaudiaB <buraanna27@gmail.com>
2023-04-09 11:32:52 -0700

    fixed semistandard error

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,12 +3,12 @@
 class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
-	    this.width = w;
-	    this.height = h;
+      this.width = w;
+      this.height = h;
     } else {
-	    this.width;
-	    this.height;
-    }
+        this.width;
+	this.height;
+      }
   }
 }
 
