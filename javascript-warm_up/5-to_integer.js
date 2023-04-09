5843a52e4bae5d11dfa6624dbd4c5a06a9f17b50
CllaudiaB <buraanna27@gmail.com>
2023-04-09 06:15:37 -0700

    fixed error semistandard

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -5,7 +5,7 @@ const arg = parseInt(process.argv[2]);
 if (arg == null) {
   console.log('Not a number');
 } else if (Number.isInteger(arg)) {
-  console.log('My number: ' +  arg);
+  console.log('My number: ' + arg);
 } else {
   console.log('Not a number');
 }
