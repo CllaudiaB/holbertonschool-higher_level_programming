047c71f6be320b0875b566214fba2613145d19c2
CllaudiaB <buraanna27@gmail.com>
2023-04-09 06:06:57 -0700

    fixed error

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-let arg = process.argv[2];
+let arg = parseInt(process.argv[2]);
 
 if (arg == null) {
     console.log('Not a number');
-} else if (parseInt(arg) == 89) {
-    console.log('My number:' ,Math.trunc(arg));
+} else if (Number.isInteger(arg)) {
+    console.log('My number:', arg);
 } else {
     console.log('Not a number');
 }
