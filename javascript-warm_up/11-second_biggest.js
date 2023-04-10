15339f34a4b85a755f31abefecfb6a0c41b16c2d
CllaudiaB <buraanna27@gmail.com>
2023-04-10 12:01:49 -0700

    fixed error

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,21 +1,26 @@
 #!/usr/bin/node
 
-const args = process.argv;
-let firstBiggest = args[2];
-let secondBiggest = args[2];
+let args = process.argv.splice(2);
+let list = [];
+for (let i = 0; i < args.length; i++) {
+  list.push(parseInt(args[i]));
+}
 
-if (args[2] === null || args[2] === 1) {
+let firstBiggest = 1;
+let secondBiggest = 1;
+
+if(args.length <= 1) {
   console.log('0');
 } else {
-  for (let i = 3; i < args.length; i++) {
-    if (args[i] > firstBiggest) {
-      firstBiggest = args[i];
+  for (let i = 0; i <= list.length; i++) {
+    if (list[i] > firstBiggest) {
+      firstBiggest = list[i];
     }
   }
-  for (let j = 3; j < args.length; j++) {
-    if (args[j] < firstBiggest && args[j] > secondBiggest) {
-      secondBiggest = args[j];
+  for (let i = 1; i <= list.length; i++) {
+    if (secondBiggest < list[i] && list[i] < firstBiggest) {
+      secondBiggest = list[i];
     }
   }
-  console.log(secondBiggest);
+    console.log(secondBiggest);
 }
