0f2ca5ecb8b8e1ef0f472c31ee18c20aeae32ff1
CllaudiaB <buraanna27@gmail.com>
2023-04-10 07:04:56 -0700

    adding file for task 6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,45 +1,22 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+const Square2 = require('./5-square');
+
+class Square extends Square2 {
+  constructor (size) {
+    super(size, size);
+    this.size = this.width;
   }
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+  charPrint (c) {
+    for (let i = 0; i < this.size; i++) {
+      if (c === undefined) {
+        console.log('X'.repeat(this.size));
+      } else {
+        console.log(c.repeat(this.size));
+      }
     }
   }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-}
-
-class Square extends Rectangle {
-    constructor (size) {
-	super(size, size);
-	this.size = size;
-    }
-    charPrint (c) {
-	for (let i = 0; i < this.size; i++) {
-	    if (c == undefined) {
-		console.log('X'.repeat(this.size));
-	    } else {
-		console.log(c.repeat(this.size));
-	    }
-	}
-    }
 }
 
 module.exports = Square;
