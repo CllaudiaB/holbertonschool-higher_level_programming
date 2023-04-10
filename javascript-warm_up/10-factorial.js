4a428a20390fde4e84bf2efcd70825ad8d68a32f
CllaudiaB <buraanna27@gmail.com>
2023-04-10 01:58:04 -0700

    fixed error

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 function fact (number) {
-    if (isNaN(number)) {
-	return 1;
-    } else {
-	return number * fact(number - 1);
-    }
+  if (!number) {
+    return 1;
+  } else {
+    return number * fact(number - 1);
+  }
 }
 
-let arg = process.argv;
-let result = fact(arg[2]);
+const arg = process.argv;
+const result = fact(arg[2]);
 console.log(result);
