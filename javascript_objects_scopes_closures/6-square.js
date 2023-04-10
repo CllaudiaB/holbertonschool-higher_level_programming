a88f62213bba2e65cc275af24fc62bd0ee8dccb6
CllaudiaB <buraanna27@gmail.com>
2023-04-10 07:16:20 -0700

    fixed error

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -5,15 +5,14 @@ const Square2 = require('./5-square');
 class Square extends Square2 {
   constructor (size) {
     super(size, size);
-      this.size = this.height;
   }
 
   charPrint (c) {
-    for (let i = 0; i < this.size; i++) {
+    for (let i = 0; i < this.height; i++) {
       if (c === undefined) {
-        console.log('X'.repeat(this.size));
+        console.log('X'.repeat(this.height));
       } else {
-        console.log(c.repeat(this.size));
+        console.log(c.repeat(this.height));
       }
     }
   }
