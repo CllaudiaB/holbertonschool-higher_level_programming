a9ebc69976c77d28fab70cdabd24f28b94707e08
CllaudiaB <buraanna27@gmail.com>
2023-04-10 09:26:43 -0700

    fixed semistandard error

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -2,10 +2,10 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-    for (let i = 0; i < list.length; i++) {
-      if (list[i] === searchElement) {
-        count += 1;
-      }
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count += 1;
     }
+  }
   return count;
-}
+};
