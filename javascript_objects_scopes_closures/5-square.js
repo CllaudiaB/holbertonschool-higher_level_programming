1b52f997aee3d9622fe0a6a69b3df5a2f8995b40
CllaudiaB <buraanna27@gmail.com>
2023-04-10 06:14:12 -0700

    adding file for task 5

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
