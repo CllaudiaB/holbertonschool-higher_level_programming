4c34bd66309bcad4cb6a6b8c9018c002b27f594f
root <root@Claudia.localdomain>
2023-04-17 13:23:25 +0200

    fixed error

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-const axios = require('axios');
-axios.get(process.argv[2])
-  .then((response) => console.log('code: ', response.status))
-  .catch((error) => console.log('code: ', error.response.status));
+require('fetch');
+fetch(process.argv[2])
+  .then(response => {
+      console.log(response.status);
+  });
