3472dfa2a26c9d0211dc6554edbe8ecef680b345
root <root@Claudia.localdomain>
2023-04-17 16:03:06 +0200

    adding file for task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,11 +4,11 @@ const request = require('request');
 const id = process.argv[2];
 
 request({
-  url: 'https://swapi-api.hbtn.io/api/films/${id}',
-  json: true
-}, (err, response, body) => {
-  if (err) {
-    console.log('Error:' + err);
-  }
-  console.log(body.title);
-});
+    url: `https://swapi-api.hbtn.io/api/films/${id}`,
+    json: true
+  }, (err, response, body) => {
+    if (err) {
+      console.log('Error:', err);
+    }
+    console.log(body.title);
+  });
