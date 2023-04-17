08fcd3b58dc63ce076d252acfa9a952c3a6bd34b
root <root@Claudia.localdomain>
2023-04-17 11:23:55 +0200

    fixed error

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const data = 'Python is cool';
+const data = process.argv[3];
 fs.writeFile(process.argv[2], data, (err) => {
   if (err) throw err;
   else console.log(data);
