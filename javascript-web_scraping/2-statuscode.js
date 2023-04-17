be7c27734b69fef6653b72b2966a892eab15dedf
root <root@Claudia.localdomain>
2023-04-17 13:04:29 +0200

    adding file for task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 
 const axios = require('axios');
 axios.get(process.argv[2])
-  .then((response) => console.log('code: ' + response.status))
-  .catch((error) => console.log('code: ' + error.response.status));
+  .then((response) => console.log('code: ', response.status))
+  .catch((error) => console.log('code: ', error.response.status));
