b7d28c09e757b15e21856c49744eb8b30d8f56ba
root <root@Claudia.localdomain>
2023-04-21 14:04:33 +0200

    adding file for task 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+let myDict = {};
+
+request({
+  url: process.argv[2],
+  json: true
+}, (err, response, body) => {
+  if (err) {
+    console.log('Error:', err);
+  }
+for (let i = 1; i < body.length; i++) {
+    if (body[i].completed == true) {
+      if (myDict[body[i].userId] === undefined) {
+        myDict[body[i].userId] = 0;
+      }
+      myDict[body[i].userId] += 1;
+    }
+}
+console.log(myDict);
+});
